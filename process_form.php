c17b79f42a31cb2b284f0dbf4c23497b8b4866e1
nsewmini <nsewmini2001@gmail.com>
2023-12-29 14:28:18 +0530

    CHANGE

--- process_form.php
+++ process_form.php
@@ -1,10 +1,15 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    // Get and sanitize form data
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
     // Set recipient email address
     $to = "sewminin3@email.com";
@@ -19,9 +24,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_message .= "Message:\n$message";
 
     // Set headers
-    $headers = "From: $email";
+    $headers = "From: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\r\n";
 
-    // Send the email
-    mail($to, $email_subject, $email_message, $headers);
+    // Send the email using PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->setFrom($email);
+        $mail->addAddress($to);
+        $mail->Subject = $email_subject;
+        $mail->Body = $email_message;
+
+        $mail->send();
+        echo "Email sent successfully!";
+    } catch (Exception $e) {
+        echo "Error sending email: {$mail->ErrorInfo}";
+    }
 }
 ?>
+
